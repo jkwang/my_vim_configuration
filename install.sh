46050ab5427f7f83e13a289ba72bc9bf6482ad4e
J.K Wang <jkun.wang@gmail.com>
2010-04-13 13:20:52 +0800

    modified install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo '
 fun! MySys()
    return "$1"
 endfun
-set runtimepath=~/.vim_runtime,~/.vim_runtime/after,\$VIMRUNTIME
+set runtimepath=~/.vim_runtime,~/.vim_runtime/after,$VIMRUNTIME
 source ~/.vim_runtime/vimrc
 helptags ~/.vim_runtime/doc' > ~/.vimrc
 echo "Installed amix's vim configuration successfully! Enjoy :)"
